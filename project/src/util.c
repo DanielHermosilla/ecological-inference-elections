fa0b04a7c1ce3070e268cedee41878f6140ac083
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2024-12-24 10:15:29 -0300

    Start and documentate the starting function, ditch GPL and use BLAS instead for more performance

--- project/src/util.c
+++ project/src/util.c
@@ -1,8 +1,37 @@
 #include <R.h>
 #include <R_ext/Rdynload.h>
 #include <Rinternals.h>
-#include <gsl/gsl_sf_bessel.h>
+#include <cblas.h>
 #include <stdio.h>
+
+// note: Refer to https://www.seehuhn.de/pages/linear.html#blas
+// double m[] = {
+// 3, 1, 3,
+// 1, 5, 9,
+// 2, 6, 5
+// };
+
+double getInitialP()
+{
+
+    /**
+     * @brief Computes the initial probability of the EM algoritm.
+     *
+     * Given the observables results, it computes a convenient initial "p" value for initiating the
+     * algorithm. Currently it supports the "uniform", "group_proportional" and "proportional" methods.
+     *
+     * @param[in] X Matrix of dimension (cxb) that stores the results of candidate "c" on ballot box "b".
+     * @param[in] w Matrix of dimension (bxg) that stores the amount of votes from the demographic group "g".
+     * @param[in] p_method The method for calculating the initial parameter. Currently it supports "uniform",
+     * "group_proportional" and "proportional" methods.
+     * @return Matrix of dimension (gxc) with the initial probability for each demographic group "g" voting for a given
+     * candidate "c".
+     * @note This should be used only that the first iteration of the EM-algorithm.
+     * @warning Warnings about misuse or side effects.
+     * @see Reference to other relevant functions or documentation.
+     */
+    return R_NilValue;
+}
 
 SEXP hello_gsl()
 {
