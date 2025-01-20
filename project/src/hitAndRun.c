3aa5431f6ee3addaef15cf04917a5498d766eb7c
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2025-01-20 12:04:19 -0300

    edit some matrix utils functions and start the main algorithm for H&R

--- project/src/hitAndRun.c
+++ project/src/hitAndRun.c
@@ -1,4 +1,5 @@
 #include "hitAndRun.h"
+#include "globals.h"
 #include <stdio.h>
 
 // ---- Define a structure to store the Omega sets ---- //
@@ -9,10 +10,44 @@ typedef struct
     size_t size;
 } Set;
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b)) // Macro for finding the minimum
+
 Set **OMEGASET = NULL; // Global pointer to store all H sets
 
-void generateOmegaSet()
+/**
+ *  @brief Yields an initial point of the polytope given a ballot
+ *
+ * Given a ballot box index, it returns a matrix of size (GxC) with a valid starting point for the Hit and Run
+ * algorithm.
+ *
+ * @param[in] b The ballot box index
+ *
+ * @return A matrix with the starting point.
+ * Description.
+ */
+Matrix startingPoint(int b)
 {
+    Matrix toReturn = createMatrix(TOTAL_GROUPS, TOTAL_CANDIDATES);
+    double *groupVotes = getRow(W, b);
+    double *candidateVotes = getColumn(X, b);
+    for (uint16_t g = 0; g < TOTAL_GROUPS; g++)
+    {
+        for (uint16_t c = 0; c < TOTAL_CANDIDATES; c++)
+        {
+            MATRIX_AT(toReturn, g, c) = MIN(groupVotes[g], candidateVotes[c]);
+            groupVotes[g] -= MATRIX_AT(toReturn, g, c);
+            candidateVotes[c] -= MATRIX_AT(toReturn, g, c);
+        }
+    }
+    free(groupVotes);
+    free(candidateVotes);
+    return toReturn;
+}
+
+void generateOmegaSet(int M, int S)
+{
+
+    int totalSamples = M * S;
     // ---- Allocate memory for the `b` index ----
     OMEGASET = malloc(TOTAL_BALLOTS * sizeof(Set *));
 
@@ -21,28 +56,39 @@ void generateOmegaSet()
         OMEGASET[b] = malloc(sizeof(Set));
         // ---- Allocate memory for the `g` index ----
         OMEGASET[b]->b = b;
+        OMEGASET[b]->size = totalSamples;
+        Matrix startingZ = startingPoint(b);
 
-        // ---- Parameters for the function ----
-        size_t totalVotes = BALLOTS_VOTES[b];
-
-        // --- ... --- //
-
-        // ---- Compute the set combinations ---- //
-        size_t count = 0;
-        printf("\nRunning for ballot:\t%d\n", b);
-        Matrix **configurations =
-            generateAllConfigurationsPerBallot(b, totalVotes, TOTAL_CANDIDATES, TOTAL_GROUPS, &count);
-
-        // ---- Store configurations and size ----
-        OMEGASET[b]->data = configurations;
-        OMEGASET[b]->size = count;
-        printf("Printing the generated configurations:\n");
-        for (size_t a = 0; a < count; a++)
+        for (int s = 0; s < S; s++)
         {
-            printMatrix(OMEGASET[b]->data[a]);
-            printf("\n");
+            Matrix steppingZ = startingZ;
+            for (int m = 0; m < M; m++)
+            {
+
+                // ---- Sample random indexes ---- //
+                int groupIndex1 = rand() % TOTAL_GROUPS;
+                int groupIndex2 = rand() % (TOTAL_GROUPS - 1);
+                if (groupIndex2 >= groupIndex1)
+                    groupIndex2++;
+
+                int candidateIndex1 = rand() % TOTAL_CANDIDATES;
+                int candidateIndex2 = rand() % (TOTAL_CANDIDATES - 1);
+                if (candidateIndex2 >= candidateIndex1)
+                    candidateIndex2++;
+                // ---...--- //
+
+                double firstSubstraction = MATRIX_AT(steppingZ, groupIndex1, candidateIndex1) - 1;
+                double secondSubstraction = MATRIX_AT(steppingZ, groupIndex2, candidateIndex2) - 1;
+
+                if (firstSubstraction < 0 || secondSubstraction < 0)
+                    continue;
+
+                MATRIX_AT(steppingZ, groupIndex1, candidateIndex1) -= 1;
+                MATRIX_AT(steppingZ, groupIndex2, candidateIndex2) -= 1;
+                MATRIX_AT(steppingZ, groupIndex2, candidateIndex1) += 1;
+                MATRIX_AT(steppingZ, groupIndex1, candidateIndex2) += 1;
+            }
         }
-        // --- ... --- //
     }
 }
 
