63ae48f86a9cc95a8a62a6877f24f94ec6e54a01
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2025-01-31 15:12:51 -0300

    add use of apple accelerate

---
+++ src/build/multivariate-pdf.d
@@ -0,0 +1,2 @@
+build/multivariate-pdf.o: multivariate-pdf.c multivariate-pdf.h globals.h \
+  utils/matrixUtils.h utils/multivariateUtils.h
---
+++ src/build/utils/matrixUtils.d
@@ -0,0 +1,2 @@
+build/utils/matrixUtils.o: utils/matrixUtils.c utils/matrixUtils.h \
+  /usr/local/opt/libomp/include/omp.h
