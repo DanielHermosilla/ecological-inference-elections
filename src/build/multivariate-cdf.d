04f89fcf8059f4797e0de9b039eea5963f1e40e6
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2025-02-02 21:33:56 -0300

    remove unnecesary threading

---
+++ src/build/multivariate-cdf.d
@@ -0,0 +1,14 @@
+build/multivariate-cdf.o: multivariate-cdf.c multivariate-cdf.h globals.h \
+  matrixUtils.h utils/multivariateUtils.h utils/matrixUtils.h \
+  /usr/local/opt/openblas/include/cblas.h \
+  /usr/local/opt/openblas/include/openblas_config.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_cdf.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_monte.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_monte_miser.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_rng.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_types.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_errno.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_inline.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_monte_plain.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_monte_vegas.h \
+  /usr/local/Cellar/gsl/2.8/include/gsl/gsl_qrng.h
