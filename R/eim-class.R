a091e39a69aa401391f43869c907025925b87ce1
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2025-05-16 15:49:50 -0400

    fix welchtest

--- R/eim-class.R
+++ R/eim-class.R
@@ -1115,8 +1115,8 @@ welchtest <- function(object1 = NULL,
         stop("Invalid input: supply only one interface: objects OR matrices, not both.")
     }
 
-    all_params <- lapply(as.list(match.call(expand.dots = TRUE)), eval, parent.frame())
-    .validate_compute(all_params) # nolint # It would validate nboot too.
+    # all_params <- lapply(as.list(match.call(expand.dots = TRUE)), eval, parent.frame())
+    # .validate_compute(all_params) # nolint # It would validate nboot too.
 
     # Retrieve default values from bootstrap() and update with user parameters
     bootstrap_defaults <- formals(bootstrap)
